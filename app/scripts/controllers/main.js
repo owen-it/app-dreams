4f981ba2a39f54d22b4f3eea694116e108dfa140
anteriovieira@gmail.com <Antrio>
2015-09-15 09:46:19 -0300

    Main controller created

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name DreamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the DreamApp
+ */
+angular.module('DreamApp')
+    .controller('MainCtrl', function ($scope, $rootScope, OAuth) {
+
+        $rootScope.info = 'Wow, how nice to see you here, I\'m looking forward to meeting your dreams.';
+
+
+    });
